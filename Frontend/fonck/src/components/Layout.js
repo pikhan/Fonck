df6158ac45cd3bfb8fbeda2c6c61e81f6763cd07
Timothy Ngo <77961406+Timothy-Ngo@users.noreply.github.com>
2023-04-15 10:14:42 -0700

    Navbar: logo

--- Frontend/fonck/src/components/Layout.js
+++ Frontend/fonck/src/components/Layout.js
@@ -2,22 +2,23 @@ import {Outlet} from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import "./Layout.css";
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import logo from "../assets/ItineroLogo.png"
 
 const Layout = () => {
     return (
-        <div>
-            <nav>
-                <ul>
-                    <li><Link to="/Home">Itinero</Link></li>
-                    <li><Link to="/BuildItinerary">Create an Itinerary!</Link></li>
-                    <li><Link to="/Profile"><AccountCircleIcon/></Link></li>
-                </ul>
+        <main>
+            <nav className="nav-el">
+                    <ul>
+                        <li className="logo-item"><Link to="/Home"><img src={logo} alt="Itinero logo" className="logo-img"/></Link></li>
+                        <li><Link to="/BuildItinerary">Create an Itinerary!</Link></li>
+                        <li><Link to="/Profile"><AccountCircleIcon/></Link></li>
+                    </ul>
             </nav>
 
             <main><Outlet/></main>
 
             <footer></footer>
-        </div>
+        </main>
     );
 };
 
