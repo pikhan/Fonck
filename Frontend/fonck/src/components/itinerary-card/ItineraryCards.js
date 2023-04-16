cff6079af4dc69f708974ad106524093d3220bcb
fchau1 <fchau@nevada.unr.edu>
2023-04-15 17:55:51 -0700

    Home and Create Itinerary UI

--- Frontend/fonck/src/components/itinerary-card/ItineraryCards.js
+++ Frontend/fonck/src/components/itinerary-card/ItineraryCards.js
@@ -1,9 +1,14 @@
 import EventNoteIcon from '@mui/icons-material/EventNote';
+import "./ItineraryCards.css"
+
 
 
 const ItineraryCard = ({cardInfo, num}) => {
+
+    const cardImgPath = `../../assets/itinerary${num}.png`;
     return(
         <div className="card-container">
+            <img src={`../../assets/itinerary${num}.png`}/>
             <h1><EventNoteIcon fontSize="large"/></h1>
             <div className="card-info">
                 <h3>Option {num}</h3>
